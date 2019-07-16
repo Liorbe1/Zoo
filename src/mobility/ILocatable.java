6d23428b25813d927ad3dffea48392be1c7e1916
Liorbe1 <liorbe92@gmail.com>
2019-07-16 12:54:32 +0300

    Update ILocatable.java

--- src/mobility/ILocatable.java
+++ src/mobility/ILocatable.java
@@ -1,9 +1,5 @@
 package mobility;
-/**
- * 
- * @author asaf binder 308240407 and lior benisty 204513386
- *
- */
+
 public interface ILocatable {
 	public Point getLocation();
 	public boolean setLocation(Point location);
