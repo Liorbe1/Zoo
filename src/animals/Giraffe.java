90720b60f8b2164b0093bb6aca63556ecdbfd32f
Liorbe1 <liorbe92@gmail.com>
2019-07-16 12:13:17 +0300

    Update Giraffe.java

--- src/animals/Giraffe.java
+++ src/animals/Giraffe.java
@@ -4,11 +4,7 @@ package animals;
 import food.IEdible;
 import utilities.MessageUtility;
 
-/**
- * 
- * @author asaf binder 308240407 and lior benisty 204513386
- *
- */
+
 public class Giraffe extends AnimalsThatChews{
 	private double neckLength;
 	/**
