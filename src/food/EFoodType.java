2e399d02df1e0d6efd753033f5c6178e2d161d88
Liorbe1 <liorbe92@gmail.com>
2019-07-16 12:17:22 +0300

    Update EFoodType.java

--- src/food/EFoodType.java
+++ src/food/EFoodType.java
@@ -1,10 +1,8 @@
 package food;
-/**
- * 
- * @author asaf binder 308240407 and lior benisty 204513386
- *
- */
-public enum EFoodType {
-	MEAT , NOTFOOD ,VEGETABLE 
 
+
+public enum EFoodType {
+	MEAT,
+	NOTFOOD,
+	VEGETABLE
 }
