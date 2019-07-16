b4b54ce64dcda0960bdb46932345fed52244150c
Liorbe1 <liorbe92@gmail.com>
2019-07-16 12:55:37 +0300

    Update Cabbage.java

--- src/plants/Cabbage.java
+++ src/plants/Cabbage.java
@@ -2,19 +2,11 @@ package plants;
 
 import graphics.IDrawable;
 import graphics.ZooPanel;
-
-
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
 import utilities.MessageUtility;
 
-/**
- * @author asaf binder 308240407 and lior benisty 204513386
- *
- */
 public class Cabbage extends Plant {
 	private static volatile Cabbage instance = null;
 	
@@ -37,8 +29,4 @@ public class Cabbage extends Plant {
 		}
 		return instance;
 	}
-
-
-	
-
 }
